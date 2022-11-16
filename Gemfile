2bc3bd6cc0795616a42970fd5a1f14b3a66742d5
Jack Burton <burtonjackpg@gmail.com>
2022-11-16 17:55:42 +0000

    finished task 2

--- Gemfile
+++ Gemfile
@@ -63,3 +63,7 @@ end
 
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
+
+# important gem told to add in for challenge in kitt
+gem "simple_form", github: "heartcombo/simple_form"
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get 'restaurants', as: 'restaurants#index'
+  # get "restaurants/:id", to: "restaurants#show"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id/edit", to: "restaurants#edit"
+  # patch "restaurants/:id", to: "restaurants#update"
 end
